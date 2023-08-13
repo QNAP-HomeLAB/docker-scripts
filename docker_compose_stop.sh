8757a941997963be4eed28b891f72393a3184f92
Drauku <drauku@gmail.com>
2023-08-13 14:32:02 -0500

    color variable checks
    add `:?` to color vars so echo will still display if vars not defined

--- docker_compose_stop.sh
+++ docker_compose_stop.sh
@@ -8,21 +8,21 @@
 
 # function definitions
   fnc_help(){
-    echo -e "${blu}[-  This script STOPS or brings DOWN a single Docker container using a pre-written compose file  -]${DEF}"
+    echo -e "${blu:?}[-  This script STOPS or brings DOWN a single Docker container using a pre-written compose file  -]${def:?}"
     echo -e " -"
     echo -e " - SYNTAX: # dcd | dcr | dcp"
-    echo -e " - SYNTAX: # dcd ${cyn}stack_name${DEF}"
-    echo -e " - SYNTAX: # dcd ${cyn}-option${DEF}"
+    echo -e " - SYNTAX: # dcd ${cyn:?}stack_name${def:?}"
+    echo -e " - SYNTAX: # dcd ${cyn:?}-option${def:?}"
     echo -e " -   VALID OPTIONS:"
-    echo -e " -     ${cyn}-h │ --help ${DEF}| Displays this help message."
-    echo -e " -     ${cyn}-a | --all  ${DEF}| Brings down all docker containers shown with the 'docker ps' command."
+    echo -e " -     ${cyn:?}-h │ --help ${def:?}| Displays this help message."
+    echo -e " -     ${cyn:?}-a | --all  ${def:?}| Brings down all docker containers shown with the 'docker ps' command."
     echo
     exit 1 # Exit script after printing help
     }
-  fnc_script_intro(){ echo -e "${blu}[-  STOPPING LISTED DOCKER CONTAINERS  -]${DEF}"; }
-  fnc_script_outro(){ echo -e "${blu}[-  LISTED DOCKER CONTAINERS ${red}STOPPED${blu}  -]${DEF}"; }
-  fnc_nothing_to_do(){ echo -e "${YLW} -> no configuration files exist${DEF}"; }
-  fnc_invalid_syntax(){ echo -e "${YLW} >> INVALID OPTION SYNTAX, USE THE -${cyn}help${YLW} OPTION TO DISPLAY PROPER SYNTAX <<${DEF}"; exit 1; }
+  fnc_script_intro(){ echo -e "${blu:?}[-  STOPPING LISTED DOCKER CONTAINERS  -]${def:?}"; }
+  fnc_script_outro(){ echo -e "${blu:?}[-  LISTED DOCKER CONTAINERS ${red}STOPPED${blu:?}  -]${def:?}"; }
+  fnc_nothing_to_do(){ echo -e "${YLW:?} -> no configuration files exist${def:?}"; }
+  fnc_invalid_syntax(){ echo -e "${YLW:?} >> INVALID OPTION SYNTAX, USE THE -${cyn:?}help${YLW:?} OPTION TO DISPLAY PROPER SYNTAX <<${def:?}"; exit 1; }
   fnc_list_processing(){ remove_list="$(for stack in "${remove_list[@]}" ; do echo "$stack" ; done | sort -u)"; }
   fnc_configs_list_all(){ IFS=$'\n' remove_list=("$(docker container list --format {{.Names}})"); }
   fnc_docker_compose_down(){ docker compose -f "${docker_compose}/${remove_list[stack]}/${remove_list[stack]}${conftype}.yml" down; }
