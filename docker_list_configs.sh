0b7fb2c51c462ecf81239a24f063f3a77fc8a17b
Drauku <drauku@gmail.com>
2023-08-26 16:06:27 -0500

    code cleanup and color var update
    added ":?" to color vars, function and case code cleanup

--- docker_list_configs.sh
+++ docker_list_configs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # external variable sources
   source /opt/docker/scripts/.color_codes.conf
   source /opt/docker/scripts/.vars_docker.conf
@@ -7,52 +8,97 @@
   unset configs_folder_list IFS
   unset configs_list IFS
   unset configs_path IFS
-  unset conftype IFS
+  unset filetype IFS
 
 # function definitions
-  fnc_help(){
-    echo -e "${blu}[-> This script lists the existing 'stackname.yml' files in the ${YLW}../swarm/${blu} or ${YLW}../compose/${blu} folder structure. <-]${DEF}"
+  fnc_help_list_configs(){
+    echo -e "${blu:?}[-> This script lists the existing 'stackname.yml' files in the ${YLW:?}../swarm/${blu:?} or ${YLW:?}../compose/${blu:?} folder structure. <-]${DEF:?}"
     echo -e " -"
-    echo -e " - SYNTAX: # dlg ${cyn}-option${DEF}"
-    echo -e " - SYNTAX: # dccfg | dcg == 'dlg ${cyn}--compose${DEF}'"
-    echo -e " - SYNTAX: # dwcfg | dwg == 'dlg ${cyn}--swarm${DEF}'"
+    echo -e " - SYNTAX: # dlg ${cyn:?}-option${DEF:?}"
+    echo -e " - SYNTAX: # dccfg | dcg == 'dlg ${cyn:?}--compose${DEF:?}'"
+    echo -e " - SYNTAX: # dwcfg | dwg == 'dlg ${cyn:?}--swarm${DEF:?}'"
     echo -e " -   VALID OPTIONS:"
-    echo -e " -     ${cyn}-h │ --help    ${DEF}│ Displays this help message."
-    echo -e " -     ${cyn}-c │ --compose ${DEF}│ Displays stacks with config files in the ${YLW}..${docker_compose}/${def} filepath."
-    echo -e " -     ${cyn}-w │ --swarm   ${DEF}│ Displays stacks with config files in the ${YLW}..${docker_swarm}/${def} filepath."
+    echo -e " -     ${cyn:?}-h │ --help    ${DEF:?}│ Displays this help message."
+    echo -e " -     ${cyn:?}-c │ --compose ${DEF:?}│ Displays stacks with config files in the ${YLW:?}..${docker_compose:-/opt/docker/compose}/${def:?} filepath."
+    echo -e " -     ${cyn:?}-w │ --swarm   ${DEF:?}│ Displays stacks with config files in the ${YLW:?}..${docker_swarm:-/opt/docker/swarm}/${def:?} filepath."
     echo -e " -"
     echo -e " -   NOTE: a valid option from above is required for this script to function"
     echo
     exit 1 # Exit script after printing help
     }
-  fnc_script_intro_compose(){ echo -e "${blu}[-> EXISTING DOCKER COMPOSE CONFIG FILES IN ${YLW}${configs_path}/${blu} <-]${DEF}"; }
-  fnc_script_intro_swarm(){ echo -e "${blu}[-> EXISTING DOCKER SWARM CONFIG FILES IN ${YLW}${configs_path}/${blu} <-]${DEF}"; }
-  fnc_nothing_to_do(){ echo -e "${YLW} -> no configuration files exist${DEF}"; }
-  fnc_invalid_syntax(){ echo -e "${YLW} >> INVALID OPTION SYNTAX, USE THE -${cyn}help${YLW} OPTION TO DISPLAY PROPER SYNTAX <<${DEF}"; exit 1; }
-  fnc_list_config_folders(){ IFS=$'\n' configs_folder_list=( $(cd "${configs_path}" && find -maxdepth 1 -type d -not -path '*/\.*' | sort | sed 's/^\.\///g') ); }
-  fnc_folder_list_cleanup(){ if [[ "${configs_folder_list[i]}" = "." ]]; then unset configs_folder_list[i]; fi; }
-  fnc_type_compose(){ configs_path="${docker_compose}"; conftype="-compose"; }
-  fnc_type_swarm(){ configs_path="${docker_swarm}"; unset conftype IFS; }
-  fnc_list_config_files(){ if [[ -f "${configs_path}"/"${configs_folder_list[i]}"/"${configs_folder_list[i]}${conftype}.yml" ]]; then configs_list="${configs_list} ${configs_folder_list[i]}"; fi; }
-  fnc_display_config_files(){ for i in "${!configs_folder_list[@]}"; do fnc_folder_list_cleanup; fnc_list_config_files; done; }
-  fnc_script_outro(){ if [[ ! ${configs_list} ]]; then echo -e " -> ${YLW}no configuration files exist${DEF}"; else echo -e " ->${cyn}${configs_list[*]}${DEF}"; fi; echo; }
+  case "$1" in ("-h"|*"help"*) fnc_help_list_configs ;; esac
+
+## function definitions
+
+  # intro message for script
+  fnc_script_intro(){ echo -e "${blu:?}[-> EXISTING DOCKER ${conftype:?}CONFIG FILES IN ${YLW:?}${configs_path}/${blu:?} <-]${DEF:?}"; }
+  # invalid syntax message
+  fnc_invalid_syntax(){ echo -e "${YLW:?} >> INVALID OPTION SYNTAX, USE THE -${cyn:?}help${YLW:?} OPTION TO DISPLAY PROPER SYNTAX <<${DEF:?}"; exit 1; }
+  # nothing to do message
+  fnc_nothing_to_do(){ echo -e "${YLW:?} -> no configuration files exist${DEF:?}"; }
+  # populate configs list array with docker config files in the '/opt/docker/${conftype}' folder
+  fnc_list_config_folders(){
+    IFS=$'\n' configs_folder_list=( $(cd "${configs_path}" && find . -maxdepth 1 -type d -not -path '*/\.*' | sort | sed 's/^\.\///g') );
+    }
+  # set script variables for Docker Compose configs
+  fnc_type_compose(){
+    conftype="COMPOSE ";
+    filetype="-compose";
+    configs_path="${docker_compose:-/opt/docker/compose}";
+    source "${docker_compose}"/.stackslist-compose.conf;
+    }
+  # set script variables for Docker Swarm configs
+  fnc_type_swarm(){
+    conftype="SWARM ";
+    filetype="-stack";
+    configs_path="${docker_swarm:-/opt/docker/swarm}";
+    source "${docker_swarm}"/.stackslist-swarm.conf;
+    }
+  # clean up configs list array
+  fnc_folder_list_cleanup(){
+    if [[ "${configs_folder_list[i]}" = "." ]]; then
+    unset "configs_folder_list[i]";
+    fi;
+    for i in "${!configs_folder_list[@]}"; do
+      if [ i == "." ];
+      then unset "configs_folder_list[i]";
+      fi;
+    done;
+    }
+  # populate the configs_list array
+  fnc_list_config_files(){
+    if [[ -f "${configs_path}"/"${configs_folder_list[i]}"/"${configs_folder_list[i]}${filetype}.yml" ]];
+    then configs_list="${configs_list} ${configs_folder_list[i]}";
+    fi;
+    }
+  fnc_display_config_files(){
+    # display config files list if any
+    if [[ ! ${configs_list} ]]; then
+      echo -e " -> ${YLW:?}no configuration files exist${DEF:?}";
+    else
+      echo -e " ->${cyn:?}${configs_list[*]}${DEF:?}";
+    fi;
+    echo;
+    }
+  fnc_script_outro(){
+    # echo -e "[-- ${GRN:?} DISPLAYED LIST OF CONFIG FILES IN ${YLW:?}${configs_path}/ ${def:?} --]";
+    echo;
+    }
 
 # determine configuration type to query
   case "$1" in
-    "-h"|"-help"|"--help")
-      fnc_help
+    ("-h"|"-help"|"--help")
+      fnc_help_list_configs
       ;;
-    "-c"|"--compose")
-      source /opt/docker/compose/.stackslist-compose.conf
+    ("-c"|"--compose")
       fnc_type_compose
-      fnc_script_intro_compose
+      fnc_script_intro
       ;;
-    "-s"|"-w"|"--swarm")
-      source /opt/docker/swarm/.stackslist-swarm.conf
+    ("-s"|"-w"|"--swarm")
       fnc_type_swarm
-      fnc_script_intro_swarm
+      fnc_script_intro
       ;;
-    *)
+    (*)
       fnc_invalid_syntax
       ;;
   esac
--- docker_system_clean.sh
+++ docker_system_clean.sh
@@ -4,56 +4,112 @@
   source /opt/docker/scripts/.vars_docker.conf
 
 # function definitions
-  fnc_help(){
-    echo -e "${blu}[-> This script cleans (removes) '${CYN}container${blu}' ${cyn}images${def}, ${cyn}volumes${def} and ${CUR}unused${def} ${cyn}networks${def} <-]${def} "
+  fnc_help_system_clean(){
+    echo -e "${blu:?}[-> This script cleans (removes) '${CYN:?}container${blu:?}' ${cyn:?}images${def:?}, ${cyn:?}volumes${def:?} and ${CUR}unused${def:?} ${cyn:?}networks${def:?} <-]${def:?} "
     echo -e " -"
     echo -e " - SYNTAX: # dclean"
-    echo -e " - SYNTAX: # dclean ${cyn}-option${def}"
+    echo -e " - SYNTAX: # dclean ${cyn:?}-option${def:?}"
     echo -e " -   VALID OPTION(S):"
-    echo -e " -     ${cyn}-a │ --all  ${def}│ Stops all containers then removes all '${CYN}container${blu}' ${cyn}images${def}, ${cyn}volumes${def} and ${CUR}unused${def} ${cyn}networks${def}"
-    echo -e " -     ${cyn}-h │ --help ${def}│ Displays this help message"
+    echo -e " -     ${cyn:?}-a │ --all  ${def:?}│ Stops all containers then removes all ${CYN:?}container${blu:?} ${cyn:?}images${def:?}, ${cyn:?}volumes${def:?} and ${cyn:?}networks${def:?}"
+    echo -e " -     ${cyn:?}-c │ --containers  ${def:?}│ Stops all containers then removes all ${CUR}exited ${CYN:?}container${def:?}"
+    echo -e " -     ${cyn:?}-i │ --images  ${def:?}│ Stops all containers then removes all ${CUR}dangling ${cyn:?}images${def:?}"
+    echo -e " -     ${cyn:?}-n │ --networks  ${def:?}│ Stops all containers then removes all ${CUR}unused ${cyn:?}networks${def:?}"
+    echo -e " -     ${cyn:?}-v │ --volumes  ${def:?}│ Stops all containers then removes all ${cyn:?}volumes${def:?}"
+    echo -e " -"
+    echo -e " -     ${cyn:?}-h │ --help ${def:?}│ Displays this help message"
     echo
     exit 1 # Exit script after printing help
     }
-  fnc_dclean(){
-    echo -e "${blu} -> REMOVE UNUSED DOCKER ${CYN}VOLUMES${DEF}";
-      #docker volume ls -qf dangling=true | xargs -r docker volume rm;
-      VOLUMES_DANGLING=$(docker volume ls -qf dangling=true);
-      if [[ ! ${VOLUMES_DANGLING} = "" ]];
-      then docker volume rm ${VOLUMES_DANGLING};
-      else echo -e " - ${YLW}No dangling volumes to remove.${DEF}";
-      fi
-      echo
-    #echo -e "${blu} -> REMOVE UNUSED DOCKER ${CYN}NETWORKS${DEF} ";
-    #  #docker network ls | grep "bridge";
-    #  NETWORKS_BRIDGED="$(docker network ls | grep "bridge" | awk '/ / { print $1 }')";
-    #  if [[ ! ${NETWORKS_BRIDGED} = "" ]];
-    #  then docker network rm ${NETWORKS_BRIDGED};
-    #  else echo -e " - No disconnected, bridged networks to remove.";
-    #  fi
-    #  echo
-    echo -e "${blu} -> REMOVE UNUSED DOCKER ${CYN}IMAGES${DEF}";
-      #docker images
-      #IMAGES_DANGLING=$(docker images --filter "dangling=true" -q --no-trunc);
-      IMAGES_DANGLING="$(docker images --filter "dangling=false" -q)";
-      if [[ ! ${IMAGES_DANGLING} = "" ]]
-      then docker rmi ${IMAGES_DANGLING};
-      else echo -e " - ${YLW}No dangling images to remove.${DEF}";
-      fi
-      #docker images | grep "none"
-      IMAGES_NONE=$(docker images | grep "none" | awk '/ / { print $3 }');
-      if [[ ! ${IMAGES_NONE} = "" ]];
-      then docker rmi ${IMAGES_NONE};
-      else echo -e " - ${YLW}No unassigned images to remove.${DEF}";
-      fi
-      echo
-    echo -e "${blu} -> REMOVE UNUSED DOCKER ${CYN}CONTAINERS${DEF}";
-      #docker ps
-      #docker ps -a
-      CONTAINERS_EXITED=$(docker ps -qa --no-trunc --filter "status=exited");
-      if [[ ! ${CONTAINERS_EXITED} = "" ]];
-      then docker rm ${CONTAINERS_EXITED};
-      else echo -e " - ${YLW}No exited containers to remove.${DEF}";
-      fi
-      echo
-    }
+  case "$1" in ("-h"|*"help"*) fnc_help_system_clean ;; esac
+
+  fnc_script_intro(){ echo -e "${blu:?}[-  ${YLW:?}STARTING${blu:?} CLEANUP OF THE DOCKER SYSTEM (CONTAINERS, IMAGES, VOLUMES, NETWORKS)  -]${def:?}"; }
+  fnc_script_outro(){ echo -e "${blu:?}[-  DOCKER SYSTEM CLEANUP ${GRN:?}COMPLETED${blu:?} -]${def:?}"; echo; }
+  fnc_nothing_to_do(){ echo -e "${YLW:?} -> No cleanup to be done.${def:?}"; }
+  fnc_invalid_syntax(){ echo -e "${YLW:?} >> INVALID OPTION SYNTAX, USE THE ${cyn:?}-help${YLW:?} OPTION TO DISPLAY PROPER SYNTAX <<${def:?}"; exit 1; }
+  fnc_operation_message(){ echo -e "${blu:?}[-> REMOVE UNUSED DOCKER ${MGN:?}${operation_type}${def:?}"; }
+  fnc_clean_volumes(){
+    fnc_operation_message;
+    #docker volume ls -qf dangling=true | xargs -r docker volume rm;
+    VOLUMES_DANGLING=$(docker volume ls -qf dangling=true);
+    if [[ ! ${VOLUMES_DANGLING} = "" ]];
+    then docker volume rm ${VOLUMES_DANGLING};
+    else echo -e " - ${YLW:?}No dangling volumes to remove.${def:?}";
+    fi
+    echo
+  }
+  fnc_clean_networks(){
+    fnc_operation_message;
+    #docker network ls | grep "bridge";
+    NETWORKS_BRIDGED="$(docker network ls | grep "bridge" | awk '/ / { print $1 }')";
+    if [[ ! ${NETWORKS_BRIDGED} = "" ]];
+    then docker network rm ${NETWORKS_BRIDGED};
+    else echo -e " - No disconnected, bridged networks to remove.";
+    fi
+    echo
+  }
+  fnc_clean_images(){
+    fnc_operation_message;
+    #docker images
+    IMAGES_DANGLING=$(docker images --filter "dangling=true" -q --no-trunc);
+    # IMAGES_DANGLING="$(docker images --filter "dangling=false" -q)";
+    if [[ ! ${IMAGES_DANGLING} = "" ]]
+    then docker rmi ${IMAGES_DANGLING};
+    else echo -e " - ${YLW:?}No dangling images to remove.${def:?}";
+    fi
+    #docker images | grep "none"
+    IMAGES_NONE=$(docker images | grep "none" | awk '/ / { print $3 }');
+    if [[ ! ${IMAGES_NONE} = "" ]];
+    then docker rmi ${IMAGES_NONE};
+    else echo -e " - ${YLW:?}No unassigned images to remove.${def:?}";
+    fi
+    echo
+  }
+  fnc_clean_containers(){
+    fnc_operation_message;
+    #docker ps
+    #docker ps -a
+    CONTAINERS_EXITED=$(docker ps -qa --no-trunc --filter "status=exited");
+    if [[ ! ${CONTAINERS_EXITED} = "" ]];
+    then docker rm ${CONTAINERS_EXITED};
+    else echo -e " - ${YLW:?}No exited containers to remove.${def:?}";
+    fi
+    echo
+  }
+
+# option logic action determination
+  case "${1}" in
+    ("")
+      fnc_invalid_syntax
+      ;;
+    (-*) # validate entered option exists
+      # fnc_script_intro
+      case "${1}" in
+        ("-a"|"--all")
+          operation_type="containers"; fnc_clean_containers
+          operation_type="images"; fnc_clean_images
+          # operation_type="networks"; fnc_clean_networks
+          # operation_type="volumes"; fnc_clean_volumes
+          ;;
+        ("-c"|"--containers")
+          operation_type="containers"; fnc_clean_containers
+          ;;
+        ("-i"|"--images")
+          operation_type="images"; fnc_clean_images
+          ;;
+        ("-n"|"--networks")
+          operation_type="networks"; fnc_clean_networks
+          ;;
+        ("-v"|"--volumes")
+          operation_type="volumes"; fnc_clean_volumes
+          ;;
+        (*)
+          fnc_invalid_syntax
+          ;;
+      esac
+      ;;
+    (*)
+      fnc_invalid_syntax
+      ;;
+  esac
+
+  fnc_script_outro
