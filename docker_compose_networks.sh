8f37556966b8bce474fcb1015a2449f6897b8766
Drauku <drauku@gmail.com>
2023-08-26 15:49:01 -0500

    color vars and minor cmd update
    added ":?" to color vars and tweaked folder creation logic, also fixed network subnet range to be within acceptable range

--- docker_compose_networks.sh
+++ docker_compose_networks.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 # external variable sources
   source /opt/docker/scripts/.script_vars.env
-  # source /opt/docker/.docker_vars.env
+  # source /opt/docker/.vars_docker.conf
 
 # function definitions
   fnc_help(){
-    echo -e "${blu}[-  This script creates Docker ${blu}networks${DEF} required for compose and swarm containers by ${CYN}Drauku${blu}  -]${DEF}"
-    echo -e " - ${blu}(modified from ${CYN}gkoerk's (RIP)${blu} famously awesome networks for stacks)${DEF}"
+    echo -e "${blu:?}[-  This script creates Docker ${blu:?}networks${def:?} required for compose and swarm containers by ${CYN:?}Drauku${blu:?}  -]${def:?}"
+    echo -e " - ${blu:?}(modified from ${CYN:?}gkoerk's (RIP)${blu:?} famously awesome networks for stacks)${def:?}"
     echo -e " -"
-    echo -e " -   SYNTAX: dnc ${cyn}-option${DEF}"
+    echo -e " -   SYNTAX: dnc ${cyn:?}-option${def:?}"
     echo -e " -     VALID OPTIONS:"
-    echo -e " -       ${cyn}-c │ --create ${DEF}│ ${grn}Creates${def} the ${blu}{netproxy,netsocket}${DEF} docker networks"
-    echo -e " -       ${cyn}-d │ --delete ${DEF}│ ${red}Deletes${def} the ${blu}{netproxy,netsocket}${DEF} docker networks"
+    echo -e " -       ${cyn:?}-c │ --create ${def:?}│ ${grn:?}Creates${def:?} the ${blu:?}{netproxy,netsocket}${def:?} docker networks"
+    echo -e " -       ${cyn:?}-d │ --delete ${def:?}│ ${RED:?}Deletes${def:?} the ${blu:?}{netproxy,netsocket}${def:?} docker networks"
   }
-  fnc_script_intro(){ echo -e "${blu}[-  CREATE DOCKER NETWORKS REQUIRED FOR USE WITH DOCKER SOCKET AND A REVERSE PROXY  -]${def}"; }
-  fnc_script_outro(){ echo -e "${GRN} -  DOCKER NETWORKS '${var_net_rproxy} & ${var_net_socket}' CREATED${DEF}"; echo; exit 1; }
-  fnc_nothing_to_do(){ echo -e "${YLW} >> A valid option and container name(s) must be entered for this command to work (use ${cyn}--help ${YLW}for info)${DEF}"; }
-  fnc_invalid_syntax(){ echo -e "${YLW} >> INVALID OPTION SYNTAX, USE THE ${cyn}-help${YLW} OPTION TO DISPLAY PROPER SYNTAX <<${DEF}"; echo; exit 1; }
-  fnc_invalid_input(){ echo -e "${YLW}INVALID INPUT${DEF}: Must be any case-insensitive variation of '(Y)es' or '(N)o'."; }
+  fnc_script_intro(){ echo -e "${blu:?}[-  CREATE DOCKER NETWORKS REQUIRED FOR USE WITH DOCKER SOCKET AND A REVERSE PROXY  -]${def:?}"; }
+  fnc_script_outro(){ echo -e "${GRN:?} -  DOCKER NETWORKS '${var_net_rproxy} & ${var_net_socket}' CREATED${def:?}"; echo; exit 1; }
+  fnc_nothing_to_do(){ echo -e "${YLW:?} >> A valid option and container name(s) must be entered for this command to work (use ${cyn:?}--help ${YLW:?}for info)${def:?}"; }
+  fnc_invalid_syntax(){ echo -e "${YLW:?} >> INVALID OPTION SYNTAX, USE THE ${cyn:?}-help${YLW:?} OPTION TO DISPLAY PROPER SYNTAX <<${def:?}"; echo; exit 1; }
+  fnc_invalid_input(){ echo -e "${YLW:?}INVALID INPUT${def:?}: Must be any case-insensitive variation of '(Y)es' or '(N)o'."; }
 
-# docker network create --driver "bridge" --opt "encrypted" --scope "local" --subnet "172.127.0.0/24" --attachable "docker_socket"
-# docker network create --driver "bridge" --opt "encrypted" --scope "local" --subnet "172.127.1.0/24" --attachable "reverse_proxy"
+# docker network create --driver "bridge" --opt "encrypted" --scope "local" --subnet "172.27.0.0/24" --gateway "172.27.0.254" --attachable "docker_socket"
+# docker network create --driver "bridge" --opt "encrypted" --scope "local" --subnet "172.27.1.0/24" --gateway "172.27.1.254" --attachable "external_edge"
+# docker network create --driver "bridge" --opt "encrypted" --scope "local" --subnet "172.27.2.0/24" --gateway "172.27.2.254" --attachable "reverse_proxy"
