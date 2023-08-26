17a5b536d0d9e2969fd3fcc5423622d95b8a25b5
Drauku <drauku@gmail.com>
2023-08-26 16:06:27 -0500

    code cleanup and color var update
    added ":?" to color vars, function and case code cleanup

--- docker_system_network.sh
+++ docker_system_network.sh
@@ -5,18 +5,18 @@
   source /opt/docker/scripts/.vars_docker.conf
 
 # function definitions
-  fnc_script_intro(){ echo -e "${blu}[-> LISTING CURRENT DOCKER NETWORKS <-]${def}";}
+  fnc_script_intro(){ echo -e "${blu:?}[-> LISTING CURRENT DOCKER NETWORKS <-]${def:?}";}
   fnc_script_outro(){ echo; exit 1; }
-  fnc_help(){
-    echo -e "${blu}[-> This script allows the user to manage docker ${cyn}networks${def} <-]${def} "
+  fnc_help_system_clean(){
+    echo -e "${blu:?}[-> This script allows the user to manage docker ${cyn:?}networks${def:?} <-]${def:?} "
     echo -e " -"
     echo -e " - SYNTAX: # dyn"
-    echo -e " - SYNTAX: # dyn ${cyn}-option${def}"
+    echo -e " - SYNTAX: # dyn ${cyn:?}-option${def:?}"
     echo -e " -   VALID OPTION(S):"
-    echo -e " -     ${cyn}-h │ --help ${def}│ Displays this help message"
+    echo -e " -     ${cyn:?}-h │ --help ${def:?}│ Displays this help message"
     fnc_script_outro
     }
-  fnc_invalid_syntax(){ echo -e "${YLW} >> INVALID OPTION SYNTAX, USE THE '${cyn}--help${YLW}' OPTION TO DISPLAY PROPER SYNTAX${DEF} <<" && fnc_script_outro; }
+  fnc_invalid_syntax(){ echo -e "${YLW:?} >> INVALID OPTION SYNTAX, USE THE '${cyn:?}--help${YLW:?}' OPTION TO DISPLAY PROPER SYNTAX${def:?} <<" && fnc_script_outro; }
   fnc_network_list(){ docker network ls; }
   # fnc_network_check_dockersocket(){ docker network ls -q --filter name="${var_net_socket}"; }
   # fnc_network_check_proxynet(){ docker network ls -q --filter name="${var_net_rproxy}"; }
@@ -32,14 +32,27 @@
     done;
     }
   fnc_network_create(){
-    if [ "${scope}" = "swarm" ] && [ ! "$(fnc_network_check_by_name "ingress")" = "" ]; then docker network rm ingress; fi;
-    if [ "${scope}" = "local" ] && [ ! "$(fnc_network_check_by_name "docker_gwbridge")" = "" ]; then docker network rm "docker_gwbridge"; fi;
-    docker network create --driver "${driver}" --opt encrypted --scope "${scope}" --subnet "${var_subnet_socket}" --attachable "${var_net_socket}";
-    docker network create --driver "${driver}" --opt encrypted --scope "${scope}" --subnet "${var_subnet_rproxy}" --attachable "${var_net_rproxy}";
-    # docker network create --driver "${driver}" --opt encrypted --scope "${scope}" --subnet "${var_subnet_exedge}" --attachable "${var_net_exedge}";
-    if [ "${scope}" = "swarm" ]; then docker network create --ingress --driver "${driver}" --opt encrypted --subnet "10.0.0.0/16" --gateway "10.0.0.254" ingress; fi;
+    # if [ "${scope}" = "swarm" ] && [ ! "$(fnc_network_check_by_name "ingress")" = "" ]; then docker network rm ingress; fi;
+    # if [ "${scope}" = "swarm" ]; then docker network create --ingress --driver "${driver}" --opt encrypted --subnet "10.0.0.0/16" --gateway "10.0.0.254" ingress; fi;
+    # if [ "${scope}" = "local" ] && [ ! "$(fnc_network_check_by_name "docker_gwbridge")" = "" ]; then docker network rm "docker_gwbridge"; fi;
+    case "${scope}" in
+      ("local")
+        if [ ! "$(fnc_network_check_by_name "docker_gwbridge")" = "" ]; then docker network rm "docker_gwbridge"; fi;
+        ;;
+      ("swarm")
+        if [ ! "$(fnc_network_check_by_name "ingress")" = "" ]; then docker network rm ingress; fi;
+        docker network create --ingress --driver overlay --opt encrypted --subnet "${var_subnet_ingress}" --gateway "${var_gateway_ingress}" "${var_net_ingress}";
+        ;;
+      (*)
+        echo -e "${YLW:?} >> ERROR: ${RED:?}INVALID NETWORK SCOPE${YLW:?} Please notify the script author <<${def:?}" && fnc_script_outro;
+        ;;
+    esac
+    docker network create --scope "${scope}" --driver "${driver}" --opt encrypted --subnet "${var_subnet_exedge}" --gateway "${var_gateway_exedge}" --attachable "${var_net_exedge}";
+    docker network create --scope "${scope}" --driver "${driver}" --opt encrypted --subnet "${var_subnet_rproxy}" --gateway "${var_gateway_rproxy}" --attachable "${var_net_rproxy}";
+    docker network create --scope "${scope}" --driver "${driver}" --opt encrypted --subnet "${var_subnet_socket}" --gateway "${var_gateway_socket}" --attachable "${var_net_socket}";
     unset "${scope}";
     unset "${driver}";
+    echo -e "\n>> DOCKER NETWORK(s) ${GRN:?}CREATED${def:?} <<" && fnc_script_outro;
     }
   fnc_network_remove(){
     docker network rm "${*}";
@@ -55,7 +68,7 @@
     (-*)
       case "${1}" in
         ("-h"|"-help"|"--help")
-          fnc_help;
+          fnc_help_system_clean;
           ;;
         ("-c"|"--compose")
           driver="bridge"
